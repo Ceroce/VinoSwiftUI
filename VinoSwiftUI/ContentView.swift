7606c7150ee03e29f0cf533d54881315ada18dcb
Renaud Pradenc <rpradenc@incwo.com>
2020-04-29 10:33:14 +0200

    Initial Commit

---
+++ VinoSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VinoSwiftUI
+//
+//  Created by Renaud Pradenc on 29/04/2020.
+//  Copyright © 2020 Céroce. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
